ad1c9d41c770bd36a3b4f79d9dcee1aeda36a995
Emyrhytmm <houkoucelik07@gmail.com>
2024-01-15 19:39:59 +0300

    Entity Configuration and Bogus

---
+++ Infrastructure/YoutubeApi.Persistance/Configurations/BrandConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Bogus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using YoutubeApi.Domain.Entities;
+
+namespace YoutubeApi.Persistance.Configurations
+{
+	public class BrandConfiguration : IEntityTypeConfiguration<Brand>
+	{
+		public void Configure(EntityTypeBuilder<Brand> builder)
+		{
+			builder.Property(x => x.Name).HasMaxLength(32);
+			Faker faker = new("tr");
+
+			Brand brand1 = new()
+			{
+				Id = 1,
+				Name = faker.Commerce.Department(),
+				CreatedDate = DateTime.Now,
+				IsDeleted = false
+			};
+
+			Brand brand2 = new()
+			{
+				Id = 2,
+				Name = faker.Commerce.Department(),
+				CreatedDate = DateTime.Now,
+				IsDeleted = false
+			}; 
+
+			Brand brand3 = new()
+			{
+				Id = 3,
+				Name = faker.Commerce.Department(),
+				CreatedDate = DateTime.Now,
+				IsDeleted = true
+			};
+
+			builder.HasData(brand1,brand2,brand3);
+		}
+	}
+}
